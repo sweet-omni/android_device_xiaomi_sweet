354849fa516035097a7e865f04ef0c0c6bc8ad0e
Ali Hasan <ahb7671@gmail.com>
2022-01-01 17:09:45 +0500

    sweet: TMP: fix kernel compilation
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := sweet_user_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
