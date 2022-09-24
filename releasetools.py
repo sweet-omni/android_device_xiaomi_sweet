4ed2f947dbdcdb9876041f74f26df35baa8515d8
Ali Hasan <ahb7671@gmail.com>
2022-09-24 08:48:38 +0500

    sweet: disable encryption
    and warn user at flash time
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -39,4 +39,6 @@ def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+  info.script.Print("WARNING: encryption is disabled!")
+  info.script.Print("Use at your own risk.")
   return
