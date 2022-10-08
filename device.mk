049e88a6dd78ccf70129eb80eaba03184fab2fdb
Ali Hasan <ahb7671@gmail.com>
2022-10-08 09:43:54 +0500

    sweet: remove XiaomiParts
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -125,8 +125,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 # Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+# PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
